8f2b0da949c06b3789b0fc53cb237a901325b981
Frank Brano Gomez <frankbr985gomes@gmail.com>
2023-01-21 23:20:13 +0530

    refactor automated technical details process in scratch.py file

---
+++ src/ddex/config.py
@@ -0,0 +1,10 @@
+"""
+Author: iamu985
+Github: https://github.com/iamu985
+
+All the project level variables, methods and classes can be kept here to be made access easy.
+"""
+import os
+
+ROOT_DIR = os.path.realpath(os.path.join(__file__, '..', '..', '..'))
+RELEASE_NOTIFICATION_SCHEMA = 'docs/assets/ern/release-notification.xsd'
